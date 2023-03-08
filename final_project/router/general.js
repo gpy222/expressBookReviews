974f928e3b85b28ae6f65931eb24d257b47416b4
gpy222 <gpy222@gmail.com>
2023-03-08 10:33:56 -0500

    work on project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,46 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  res.send(book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  let booksByAuthor = [];
+  for (var i = 0; i < bookKeys.length; i++) {
+    if (books[bookKeys[i]]['author'] === author) {
+        booksByAuthor.push(books[bookKeys[i]]);
+    }
+  }
+  res.send(JSON.stringify(booksByAuthor, null,4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const bookKeys = Object.keys(books);
+    let booksWithTitle = [];
+    for (var i = 0; i < bookKeys.length; i++) {
+      if (books[bookKeys[i]]['title'] === title) {
+        booksWithTitle.push(books[bookKeys[i]]);
+      }
+    }
+    res.send(JSON.stringify(booksWithTitle, null,4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const isbn = req.params.isbn;
+res.send(books[isbn]['reviews']);
 });
 
 module.exports.general = public_users;
